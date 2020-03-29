6423cf7b2ad31e804573bfea7c99960e4a834db0
Alex Liu <59286631+ahtlau@users.noreply.github.com>
2020-03-29 20:06:39 +0100

    Add files via upload

--- Day 2 Basic Probability/probability.R
+++ Day 2 Basic Probability/probability.R
@@ -1,12 +1,10 @@
 # Author: Alex Liu
 # Last modified: 2020-03-29
-# Description: This script finds the the probility that the sum of two fair dices is no more than 10
+# Description: This script finds the probility that the sum of two fair dices is no more than 10
 
 # Setup -------------------------------------------------------------------
 
 remove(list = ls())
-
-setwd("/Users/ahtlau/Library/Mobile Documents/com~apple~CloudDocs/Data/HackerRank - 10 Days of Statistics/Day 2 Basic Probability")
 
 library(dplyr)
 library(stringr)
@@ -24,8 +22,7 @@ n_favourable <- n_all %>%
   filter(total <= 9)
 
 # calculate probability
-(nrow(n_favourable)/nrow(n_all) %>% 
-    fractions())
+fractions(nrow(n_favourable)/nrow(n_all))
 
 
 
